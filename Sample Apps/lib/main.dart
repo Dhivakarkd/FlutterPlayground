6f58a693f7740dc776c14cb858cdb7ca5ff925d6
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2024-04-17 15:46:51 +0530

    another reorganizing

--- Sample Apps/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:flutter_playground/FilePickerDemo.dart';
-import 'package:flutter_playground/RandomTextApp.dart';
-
-main() => runApp(RandomTextApp());
